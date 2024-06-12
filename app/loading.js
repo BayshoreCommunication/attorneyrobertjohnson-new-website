946dcb344b4f47daaf8ed5410123d61a52409e8a
nuruzaman-milon <njmilon1@gmail.com>
2024-06-12 17:41:34 +0600

    loader added

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import { Spinner } from "@nextui-org/react";
+
+export default function Loading() {
+  return (
+    <div className="w-full h-[90vh] flex items-center justify-center">
+      <Spinner label="Loading..." color="primary" labelColor="primary" />
+    </div>
+  );
+}
