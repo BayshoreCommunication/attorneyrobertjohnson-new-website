b99e463163e35325ee8fdd68215e68554e56a767
arsahak <arsahak99@gmail.com>
2024-06-26 18:51:50 +0600

    update layout

--- components/shared/SectionLayout.jsx
+++ components/shared/SectionLayout.jsx
@@ -1,13 +1,15 @@
-const SectionLayout = ({ children, img, bg }) => {
+import React from 'react';
+
+const SectionLayout = React.memo(({ children, img = '', bg = '' }) => {
   return (
     <section
-      className={`${img} ${bg} mx-auto flex justify-center items-center w-full `}
+      className={`${img} ${bg} mx-auto flex justify-center items-center w-full`}
     >
       <div className='container py-[1rem] md:py-[3.5rem] mx-5 lg:mx-0'>
         {children}
       </div>
     </section>
   );
-};
+});
 
 export default SectionLayout;
