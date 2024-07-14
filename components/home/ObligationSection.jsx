8760ab17b08ca1fb949882dea30646a2bc1ea414
arsahak <arsahak99@gmail.com>
2024-07-14 14:42:29 +0600

    update home page

--- components/home/ObligationSection.jsx
+++ components/home/ObligationSection.jsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import SectionLayout from '../shared/SectionLayout';
 import CardMotion from '../motion/CardMotion';
 
@@ -22,12 +22,41 @@ const variants = {
   },
 };
 
+const obligations = [
+  {
+    id: 1,
+    defaultImage: '/images/one.png',
+    hoverImage: '/images/obligation1.png',
+    text: 'FREE NO-OBLIGATION CONSULTATION',
+  },
+  {
+    id: 2,
+    defaultImage: '/images/two.png',
+    hoverImage: '/images/obligation2.png',
+    text: 'SUPERIOR COMMUNICATION',
+  },
+  {
+    id: 3,
+    defaultImage: '/images/three.png',
+    hoverImage: '/images/obligation3.png',
+    text: 'AGGRESSIVE ADVOCACY',
+  },
+  {
+    id: 4,
+    defaultImage: '/images/four.png',
+    hoverImage: '/images/obligation4.png',
+    text: 'NO UPFRONT FEE OR COSTS',
+  },
+  {
+    id: 5,
+    defaultImage: '/images/five.png',
+    hoverImage: '/images/obligation5.png',
+    text: `IF DON'T WIN, NO ATTORNEY’S FEES`,
+  },
+];
+
 const ObligationSection = () => {
-  const [isHovered1, setIsHovered1] = useState(false);
-  const [isHovered2, setIsHovered2] = useState(false);
-  const [isHovered3, setIsHovered3] = useState(false);
-  const [isHovered4, setIsHovered4] = useState(false);
-  const [isHovered5, setIsHovered5] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
     <SectionLayout>
@@ -45,16 +74,12 @@ const ObligationSection = () => {
             y: 100,
           }}
         >
-          <h2
-            className={`text-stone-950 font-bold text-2xl md:text-5xl mt-0 mb-5 text-center`}
-          >
+          <h2 className='text-stone-950 font-bold text-2xl md:text-5xl mt-0 mb-5 text-center'>
             MY OBLIGATION TO YOU
           </h2>
-
           <p className='text-base text-stone-950 text-center'>
             Robert has over 20 years of valuable experience in the legal
-            community and insurance industry. 
-            <br />
+            community and insurance industry. <br />
             He is not afraid to fight for justice for you and your family.
           </p>
         </CardMotion>
@@ -72,136 +97,32 @@ const ObligationSection = () => {
           }}
         >
           <div className='grid grid-cols-2 md:grid-cols-5 gap-8 items-stretch mt-16'>
-            <div className=''>
-              <motion.div
-                variants={variants}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className='p-6 rounded-xl w-[120px] mx-auto hover:bg-[#4B93FF] border-2 border-slate-500 hover:border-[#4B93FF]'
-                onMouseEnter={() => setIsHovered1(true)}
-                onMouseLeave={() => setIsHovered1(false)}
-              >
-                <Image
-                  width={80}
-                  height={50}
-                  src={
-                    isHovered1 ? '/images/obligation1.png' : '/images/one.png'
-                  }
-                  alt='about img'
-                />
-              </motion.div>
-              <h2
-                className={`text-stone-950 font-bold text-md md:text-xl mt-5 mb-0 text-center`}
-              >
-                FREE NO-OBLIGATION <br />
-                CONSULTATION
-              </h2>
-            </div>
-            <div>
-              <motion.div
-                variants={variants}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className='p-6 rounded-xl w-[120px] mx-auto hover:bg-[#4B93FF] border-2 border-slate-500 hover:border-[#4B93FF]'
-                onMouseEnter={() => setIsHovered2(true)}
-                onMouseLeave={() => setIsHovered2(false)}
-              >
-                <Image
-                  width={80}
-                  height={50}
-                  src={
-                    isHovered2 ? '/images/obligation2.png' : '/images/two.png'
-                  }
-                  alt='about img'
-                />
-              </motion.div>
-              <h2
-                className={`text-stone-950 font-bold text-md md:text-xl mt-5 mb-0 text-center`}
-              >
-                SUPERIOR <br />
-                COMMUNICATION
-              </h2>
-            </div>
-            <div className=''>
-              <motion.div
-                variants={variants}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className='p-6 rounded-xl w-[120px] mx-auto hover:bg-[#4B93FF] border-2 border-slate-500 hover:border-[#4B93FF]'
-                onMouseEnter={() => setIsHovered3(true)}
-                onMouseLeave={() => setIsHovered3(false)}
-              >
-                <Image
-                  width={80}
-                  height={50}
-                  src={
-                    isHovered3 ? '/images/obligation3.png' : '/images/three.png'
-                  }
-                  alt='about img'
-                />
-              </motion.div>
-              <h2
-                className={`text-stone-950 font-bold text-md md:text-xl mt-5 mb-0 text-center`}
-              >
-                AGGRESSIVE
-                <br />
-                ADVOCACY
-              </h2>
-            </div>
-            <div className=''>
-              <motion.div
-                variants={variants}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className='p-6 rounded-xl w-[120px] mx-auto hover:bg-[#4B93FF] border-2 border-slate-500 hover:border-[#4B93FF]'
-                onMouseEnter={() => setIsHovered4(true)}
-                onMouseLeave={() => setIsHovered4(false)}
-              >
-                <Image
-                  width={80}
-                  height={50}
-                  src={
-                    isHovered4 ? '/images/obligation4.png' : '/images/four.png'
-                  }
-                  alt='about img'
-                />
-              </motion.div>
-              <h2
-                className={`text-stone-950 font-bold text-md md:text-xl mt-5 mb-0 text-center `}
-              >
-                NO UPFRONT <br />
-                FEE OR COSTS
-              </h2>
-            </div>
-            <div className=''>
-              <motion.div
-                variants={variants}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className='p-6 rounded-xl w-[120px] mx-auto hover:bg-[#4B93FF] border-2 border-slate-500 hover:border-[#4B93FF]'
-                onMouseEnter={() => setIsHovered5(true)}
-                onMouseLeave={() => setIsHovered5(false)}
-              >
-                <div className='w-[50px] ml-2'>
+            {obligations.map((item, index) => (
+              <div key={item.id}>
+                <motion.div
+                  variants={variants}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
+                  className='p-6 rounded-xl w-[120px] mx-auto hover:bg-[#4B93FF] border-2 border-slate-500 hover:border-[#4B93FF] flex items-center justify-center'
+                  onMouseEnter={() => setHoveredIndex(index)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
                   <Image
-                    width={60}
+                    width={item.id === 5 ? 50 : 80}
                     height={50}
                     src={
-                      isHovered5
-                        ? '/images/obligation5.png'
-                        : '/images/five.png'
+                      hoveredIndex === index
+                        ? item.hoverImage
+                        : item.defaultImage
                     }
-                    alt='about img'
+                    alt='Obligation img'
                   />
-                </div>
-              </motion.div>
-              <h2
-                className={`text-stone-950 font-bold text-md md:text-xl mt-5 mb-0 text-center`}
-              >
-                {`  IF DON’T WIN,`} <br />
-                {` NO ATTORNEY’S FEES`}
-              </h2>
-            </div>
+                </motion.div>
+                <h2 className='text-stone-950 font-bold text-md md:text-xl mt-5 mb-0 text-center'>
+                  {item.text}
+                </h2>
+              </div>
+            ))}
           </div>
         </CardMotion>
       </div>
